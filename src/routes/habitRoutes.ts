e1ae6a5f7428ed033bbe3ae66e6b667a39718999
Krish Ramani <krishramani03@gmail.com>
2026-06-11 23:36:02 +0530

    learned how to route using express js

---
+++ src/routes/habitRoutes.ts
@@ -0,0 +1,40 @@
+import { Router } from 'express'
+import { z } from 'zod'
+import { validateBody, validateParams } from '../middleware/validation.ts'
+
+const router = Router()
+
+const createHabitSchema = z.object({
+  name: z.string(),
+})
+
+const completeParamsSchema = z.object({
+  id: z.string().max(3),
+})
+
+router.get('/', (req, res) => {
+  res.json({ message: 'habits' })
+})
+
+router.get('/:id', (req, res) => {
+  res.json({ message: 'got one habit' })
+})
+
+router.post('/', validateBody(createHabitSchema), (req, res) => {
+  res.json({ message: 'created a habit' }).status(201)
+})
+
+router.delete('/:id', (req, res) => {
+  res.json({ message: 'deleted a habit' })
+})
+
+router.post(
+  '/:id/complete',
+  validateParams(completeParamsSchema),
+  validateBody(createHabitSchema),
+  (req, res) => {
+    res.json({ message: 'completed habit' }).status(201)
+  },
+)
+
+export default router
