fe5a8c89871dde4fbd759296f7ec0ef746bcecda
Krish Ramani <krishramani03@gmail.com>
2026-06-11 23:36:26 +0530

    how to use enviroment variables in the backend

--- env.ts
+++ env.ts
@@ -4,7 +4,7 @@ import { z } from 'zod'
 process.env.APP_STAGE = process.env.APP_STAGE || 'dev'
 
 const isProduction = process.env.APP_STAGE === 'production'
-const isDevelopment = process.env.APP_STAGE === 'dev'
+const isDevelopment = process.env.APP_STAGE === 'development'
 const isTesting = process.env.APP_STAGE === 'test'
 
 if (isDevelopment) {
@@ -17,16 +17,13 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
-
   APP_STAGE: z.enum(['dev', 'test', 'production']).default('dev'),
-
   PORT: z.coerce.number().positive().default(3000),
-  DATABASE_URL: z.string().startsWith('postgresql://'),
-  JWT_SECRET: z.string().min(32, 'Must be 32 chars long'),
+  // DATABASE_URL: z.string().startsWith('postgres://'),
+  // JWT_SECRET: z.string().min(32, 'Must be 32 characters long'),
   JWT_EXPIRES_IN: z.string().default('7d'),
   BCRYPT_ROUNDS: z.coerce.number().min(10).max(20).default(12),
 })
-
 export type Env = z.infer<typeof envSchema>
 let env: Env
 
@@ -34,12 +31,12 @@ try {
   env = envSchema.parse(process.env)
 } catch (e) {
   if (e instanceof z.ZodError) {
-    console.log('Invalid env var')
+    console.log('Invalid env variable')
     console.error(JSON.stringify(e.flatten().fieldErrors, null, 2))
 
     e.issues.forEach((err) => {
       const path = err.path.join('.')
-      console.log(`${path}: ${err.message}`)
+      console.log(`${path}:${err.message}`)
     })
 
     process.exit(1)
