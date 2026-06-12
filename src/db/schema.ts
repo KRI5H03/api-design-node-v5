4cad809f6becd33c095f7e67ccaad1d19e8b3790
Krish Ramani <krishramani03@gmail.com>
2026-06-12 09:29:07 +0530

    made the basic shema and relations

---
+++ src/db/schema.ts
@@ -0,0 +1,100 @@
+import {
+  pgTable,
+  uuid,
+  varchar,
+  text,
+  timestamp,
+  boolean,
+  integer,
+} from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
+
+export const users = pgTable('users', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  email: varchar('email', { length: 235 }).notNull().unique(),
+  username: varchar('username', { length: 50 }).notNull().unique(),
+  password: varchar('password', { length: 235 }).notNull(),
+  firstName: varchar('first_name', { length: 50 }),
+  lastName: varchar('last_name', { length: 50 }),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  upatedAt: timestamp('upated_at').notNull().defaultNow(),
+})
+
+export const habits = pgTable('habits', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  userId: uuid('user_id')
+    .references(() => users.id, { onDelete: 'cascade' })
+    .notNull(),
+  name: varchar('name', { length: 100 }).notNull(),
+  description: text('description'),
+  frequency: varchar('frequency', { length: 20 }).notNull(),
+  target: integer('target').default(1),
+  isActive: boolean('is_active').default(true).notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  upatedAt: timestamp('upated_at').notNull().defaultNow(),
+})
+
+export const entries = pgTable('entries', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  habitId: uuid('habit_id')
+    .references(() => habits.id, {
+      onDelete: 'cascade',
+    })
+    .notNull(),
+  completionDate: timestamp('completion_date').notNull().defaultNow(),
+  note: text('note'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+})
+
+export const tags = pgTable('tags', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  name: varchar('name', { length: 50 }).notNull().unique(),
+  color: varchar('color', { length: 7 }).default('#6b7280'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  upatedAt: timestamp('upated_at').notNull().defaultNow(),
+})
+
+export const habitTags = pgTable('habit_tags', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  habitId: uuid('habit_id')
+    .references(() => habits.id, {
+      onDelete: 'cascade',
+    })
+    .notNull(),
+  tagId: uuid('tage_id')
+    .references(() => tags.id, { onDelete: 'cascade' })
+    .notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+})
+
+export const userRelationships = relations(users, ({ many }) => ({
+  habits: many(habits),
+}))
+
+export const habitsRelationships = relations(habits, ({ one, many }) => ({
+  user: one(users, { fields: [habits.userId], references: [users.id] }),
+  entries: many(entries),
+  habitTags: many(habitTags),
+}))
+
+export const entriesRelations = relations(entries, ({ one }) => ({
+  habits: one(habits, {
+    fields: [entries.habitId],
+    references: [habits.id],
+  }),
+}))
+
+export const tagsRelations = relations(tags, ({ many }) => ({
+  habitTags: many(habitTags),
+}))
+
+export const habitTagsRelations = relations(habitTags, ({ one }) => ({
+  habits: one(habits, {
+    fields: [habitTags.habitId],
+    references: [habits.id],
+  }),
+  tags: one(tags, {
+    fields: [habitTags.tagId],
+    references: [tags.id],
+  }),
+}))
